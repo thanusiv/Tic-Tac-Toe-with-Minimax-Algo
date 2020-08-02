d1240097db57696c1e6b2b2a80784844ebd9276f
thanusiv <tsivakar@uwaterloo.ca>
2020-08-02 12:44:15 -0400

    added check for if game is finished

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -6,6 +6,8 @@ class TicTacToe:
         self.board = [[0, 0, 0] for i in range(3)]
         self.whos_turn = Player.X
         self.available_moves = [[x, y] for x in range(3) for y in range(3)]
+        self.game_over = False
+        self.player_won = None
 
     def play_move(self, row, column):
         if row < 0 or row > 2 or column < 0 or column > 2:
@@ -31,3 +33,49 @@ class TicTacToe:
 
     def get_available_moves(self):
         return self.available_moves
+
+    def is_game_over(self):
+        if len(self.available_moves) > 3:
+            return False
+        
+        # Check for row wins
+        for row in range(3):
+            possible_player_to_win = self.board[row, 0]
+            if possible_player_to_win == 0:
+                continue
+            row_win = True
+            for column in range(1,3):
+                if self.board[row, column] != possible_player_to_win:
+                    row_win = False
+                    break
+            if row_win:
+                self.player_won = Player.X if possible_player_to_win == 'X' else Player.O
+                return True 
+
+        # Check for column wins
+        for column in range(3):
+            possible_player_to_win = self.board[0, column]
+            if possible_player_to_win == 0:
+                continue
+            column_win = True
+            for row in range(1,3):
+                if self.board[row, column] != possible_player_to_win:
+                    column_win = False
+                    break
+            if column_win:
+                self.player_won = Player.X if possible_player_to_win == 'X' else Player.O
+                return True
+
+        # Check for left to right diagonal win
+        possible_player_to_win = self.board[0][0]
+        if self.board[1,1] == possible_player_to_win and self.board[2,2] == possible_player_to_win:
+            self.player_won = Player.X if possible_player_to_win == 'X' else Player.O
+            return True
+
+        # Check for right to left diagonal win
+        possible_player_to_win = self.board[0][2]
+        if self.board[1,1] == possible_player_to_win and self.board[2,0] == possible_player_to_win:
+            self.player_won = Player.X if possible_player_to_win == 'X' else Player.O
+            return True
+        
+        return False # if no other case is True, it must be False
